7ed056b20bc2cec98775a38bfd71a5508860cada
Andreas Kennedy <andreas@doomsday.no>
2020-02-26 11:58:46 +0100

    Actually return the WadFile after reading

--- src/polyvalence/io/Reader.hx
+++ src/polyvalence/io/Reader.hx
@@ -5,20 +5,20 @@ import sys.FileSystem;
 import sys.io.FileInput;
 import sys.io.File;
 import haxe.io.Bytes;
+
 using polyvalence.io.MacCompat;
 
 class Reader {
-
 	public static function readWad(path:String):WadFile {
-        var file = File.read(path);
-        file.bigEndian = true;
-        var fileSize = FileSystem.stat(path).size;
+		var file = File.read(path);
+		file.bigEndian = true;
+		var fileSize = FileSystem.stat(path).size;
 
-        trace("Reading " + path);
-        var fork_start = file.tryMacHeader(fileSize);
-        new WadFile(file, fork_start);
-        
-        file.close();
-        return null;
+		trace("Reading " + path);
+		var fork_start = file.tryMacHeader(fileSize);
+		var wad = new WadFile(file, fork_start);
+
+		file.close();
+		return wad;
 	}
 }
